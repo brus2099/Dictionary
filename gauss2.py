22cbe77eabd791f34a5086704d4eb4ba725a97ed
@brus2099 <indiebrus@gmail.com>
2019-11-03 23:37:04 -0800

    Determinante para 2x2 y 3x3

--- gauss2.py
+++ gauss2.py
@@ -18,8 +18,20 @@ for i in range (0, row):
         print(matriz[i][j], " ", end="")
     print("|", src[i], "|")
 
-print()
-print("Comienza la busqueda de soluciones mediante el metodo de Gauss-Jordan ")
+if row == 2:
+    det = matriz[0][0]*matriz[1][1] - matriz[1][0]*matriz[0][1]
+    if det == 0:
+        print("Matriz no tiene solucion")
+    else:
+        print()
+        print("Comienza la busqueda de soluciones mediante el metodo de Gauss-Jordan ")
+elif row == 3:
+    det = ((matriz[0][0]*matriz[1][1]*matriz[2][2]) + (matriz[1][0]*matriz[2][1]*matriz[0][2]) + (matriz[2][0]*matriz[0][1]*matriz[1][2])) - ((matriz[2][0]*matriz[1][1]*matriz[0][2]) + (matriz[2][1]*matriz[1][2]*matriz[0][0]) + (matriz[2][2]*matriz[1][0]*matriz[0][1]))
+    if det == 0:
+        print("Matriz no tiene solucion")
+    else:
+        print()
+        print("Comienza la busqueda de soluciones mediante el metodo de Gauss-Jordan ")
 
 def printMatriz():
     print("Matriz actual: ")
---
+++ newtonraphson.py
@@ -0,0 +1,67 @@
+propuesta = [2, 1]
+
+
+def matrizDerivadas(prop):
+    return [[2*prop[0], 2*prop[1]], [prop[1], prop[0]]]
+
+
+def matrizFunciones(prop):
+    return [-(prop[0]**2)-(prop[1]**2)+3, -(prop[0]*prop[1])+1]
+
+
+print(matrizDerivadas(propuesta))
+print(matrizFunciones(propuesta))
+
+def muestraMatriz(deriv, func):
+    print("Matriz actual: ")
+    for i in range (0, 2):
+        print("| ", end="")    
+        for j in range (0, 2):
+            print(deriv[i][j], " ", end="")
+        print("|", func[i], "|")
+
+def gauss(deriv, func):
+    for iter1 in range(0, 2):
+        if deriv[iter1][iter1] != 0:
+            muestraMatriz(deriv, func)
+        else:
+            print("Elemento de la columna principal != 0 mediante intercambio de renglones")
+            auxCambio = []
+            for eval1 in range (0, 2):
+                auxCambio.append(deriv[iter1][eval1])
+                deriv[iter1][eval1] = deriv[iter1+1][eval1]
+                deriv[iter1+1][eval1] = auxCambio[eval1]
+            axuSoluc = func[iter1]
+            func[iter1] = func[iter1+1]
+            func[iter1+1] = axuSoluc
+            muestraMatriz(deriv, func)
+
+        print()
+        # Volver elemento de matrix principal == 1
+        if deriv[iter1][iter1] == 1:
+            muestraMatriz(deriv, func)
+        else:
+            print("Creacion de un pivote == 1 mediante multiplicacion con un escalar.")
+            auxPivote = deriv[iter1][iter1]
+            for eval2 in range (0, 2):
+                deriv[iter1][eval2] = deriv[iter1][eval2] / auxPivote
+            func[iter1] = func[iter1]/auxPivote
+            muestraMatriz(deriv, func)
+
+        auxFila = []
+        for agreFila in range (0, 2):
+            auxFila.append(deriv[iter1][agreFila])
+        auxSolFila = func[iter1]
+        for avance1 in range (0, 2):
+            auxMulti = deriv[avance1][iter1]
+            guardavalores = []
+            for avance2 in range (0, 2):
+                guardavalores.append(deriv[avance1][avance2] - auxMulti*auxFila[avance2])
+                deriv[avance1][avance2] = guardavalores[avance2]
+            func[avance1] = func[avance1] - auxMulti*auxSolFila
+        deriv[iter1] = auxFila
+        func[iter1] = auxSolFila
+        muestraMatriz(deriv, func)
+
+muestraMatriz(matrizDerivadas(propuesta), matrizFunciones(propuesta))
+gauss(matrizDerivadas(propuesta), matrizFunciones(propuesta))
