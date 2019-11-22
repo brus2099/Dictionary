bb1f579ff8d5a6414204c2aa63f3e09468913df2
@brus2099 <indiebrus@gmail.com>
2019-11-21 20:12:21 -0600

    Cambio en Lagrange para usuario

--- 3ra_serie/3-SimpsonV3.py
+++ 3ra_serie/3-SimpsonV3.py
@@ -4,19 +4,21 @@ from sympy.utilities.lambdify import lambdify, implemented_function
 from sympy import Function
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy import *
 
-numPoint = int(input("How many point?: "))
+x = symbols('x')
 point = []
 
+numPoint = int(input("¿Cuantos puntos contendrá el polinomio?: "))
+
 for i in range (0, numPoint):
-    print("point", i, "in x:")
-    v1 = int(input())
-    print("point", i, "in y:")
-    v2 = int(input())
+    print("Introduce la coordenada del punto", i, "en x:")
+    v1 = float(input())
+    print("Introduce la coordenada del punto", i, "in y:")
+    v2 = float(input())
     point.append([v1, v2])
-    
-print("lenPoints:", len(point))
-print("Points: ", point)
+
+print("Los puntos por los que pasa la función son: ", point)
 
 run = point
 lenP = len(run)
@@ -25,9 +27,6 @@ den = []
 lis = []
 multi = 1
 restaDen = 1
-
-from sympy import *
-x = symbols('x')
 
 for i in range (0, lenP):
     for j in range (0, lenP):
@@ -38,7 +37,6 @@ for i in range (0, lenP):
     varN = expand(multi*run[i][1])
     for b in range (0, lenP):
         den.append(run[i][0]-run[b][0])
-        print("den", den)
     den.pop(i)
     for o in range (0, lenP-1):
         restaDen = restaDen * den[o]
@@ -54,27 +52,18 @@ for i in range (0, lenP):
 suma = 0
 for u in range (0, lenP):
     suma = suma + lis[u][0]
-print(suma)
+print("El polinomio generado es:", suma)
 
 f = lambdify(x, suma, 'numpy')
+
 x = np.linspace(-10, 10, 100)
 aa = f(x)
+
 plt.figure(figsize=(7, 7))
 plt.axis([-10, 10, -10, 10])
 plt.plot(x, aa, color="red")
+
 for p in range (0, lenP):
     plt.plot(run[p][0], run[p][1], marker='.', color='blue')
+
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
